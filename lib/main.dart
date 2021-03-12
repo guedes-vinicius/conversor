c118bc37f7fe0766a069e999dd2546c572d22b6c
vinicius777 <45599205+vinicius777@users.noreply.github.com>
2021-03-12 11:24:38 -0300

    Add files via upload
    Completion of bitcoin insertion

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
-import 'dart:async';
-import 'dart:io';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(MaterialApp(home: Home()));
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=48cd1586";
+
+void main() async {
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.amber,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
 
 class Home extends StatefulWidget {
@@ -14,168 +34,157 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final _ToDoController = TextEditingController();
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+  final bitcoinController = TextEditingController();
 
-  List _toDolist = [];
-  Map<String, dynamic> _lastRemoved;
-  int _lastRemovedPos;
+  double dolar;
+  double euro;
+  double bitcoin;
+  double variant;
+  double BitDolar;
 
-  @override
-  void initState() {
-    super.initState();
-    _readData().then((data) {
-      setState(() {
-        _toDolist = json.decode(data);
-      });
-    });
+  void _clearAll() {
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+    bitcoinController.text = "";
   }
 
-  void _addToDo() {
-    setState(() {
-      Map<String, dynamic> newToDo = Map();
-      newToDo["title"] = _ToDoController.text;
-      _ToDoController.text = "";
-      newToDo["ok"] = false;
-      _toDolist.add(newToDo);
-      _saveData();
-    });
+  void _realChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+    bitcoinController.text = (real / bitcoin).toStringAsFixed(7);
   }
 
-  Future<Null> _refresh() async {
-    await Future.delayed(Duration(seconds: 1));
-    setState(() {
-      _toDolist.sort((a, b) {
-        if (a['ok'] && !b['ok'])
-          return 1;
-        else if (!a['ok'] && b['ok'])
-          return -1;
-        else
-          return 0;
-      });
-      _saveData();
-    });
-    return null;
+  void _dolarChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text); // calculo bitcoin
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+    if (dolar == 1.0) {
+      bitcoinController.text =
+          (dolar * this.dolar / bitcoin).toStringAsFixed(6);
+    } else {
+      bitcoinController.text =
+          (dolar * this.dolar / bitcoin + 0.000001).toStringAsFixed(6);
+    }
+  }
+
+  void _euroChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text); // calculo bitcoin
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+    bitcoinController.text = (euro * this.euro / bitcoin).toStringAsFixed(6);
+  }
+
+  void _bitcoinChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double bitcoin = double.parse(text);
+
+    dolarController.text = (BitDolar * bitcoin).toStringAsFixed(2);
+    euroController.text = (bitcoin * this.bitcoin / euro).toStringAsFixed(2);
+    realController.text = (bitcoin * this.bitcoin).toStringAsFixed(2);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("Lista de Tarefas"),
-        backgroundColor: Color(0xFF7D79D0),
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _ToDoController,
-                    decoration: InputDecoration(
-                        labelText: "Nova Tarefa",
-                        labelStyle: TextStyle(color: Color(0xFF7D79D0))),
-                  ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+            case ConnectionState.none:
+              return Center(
+                child: Text(
+                  "Carregando Dados...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
                 ),
-                // ignore: deprecated_member_use
-                RaisedButton(
-                  color: Color(0xFF7D79D0),
-                  child: Text("ADD"),
-                  textColor: Colors.white,
-                  onPressed: _addToDo,
-                )
-              ],
-            ),
-          ),
-          Expanded(
-              child: RefreshIndicator(
-            onRefresh: _refresh,
-            child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
-                itemCount: _toDolist.length,
-                itemBuilder: buildItem),
-          ))
-        ],
-      ),
-    );
-  }
+              );
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro ao carregar dados :(",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                bitcoin = snapshot.data["results"]["bitcoin"]["mercadobitcoin"]
+                    ["buy"];
+                BitDolar =
+                    snapshot.data["results"]["bitcoin"]["coinbase"]["last"];
+                variant = snapshot.data["results"]["bitcoin"]["mercadobitcoin"]
+                    ["variation"];
 
-  Widget buildItem(BuildContext context, int index) {
-    return Dismissible(
-      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-      background: Container(
-        color: Colors.red,
-        child: Align(
-          alignment: Alignment(-0.9, 0.0),
-          child: Icon(
-            Icons.delete,
-            color: Colors.white,
-          ),
-        ),
-      ),
-      direction: DismissDirection.startToEnd,
-      child: CheckboxListTile(
-        title: Text(_toDolist[index]["title"]),
-        value: _toDolist[index]["ok"],
-        activeColor: Color(0xFF7D79D0),
-        secondary: CircleAvatar(
-          child: Icon(_toDolist[index]["ok"] ? Icons.check : Icons.error),
-          foregroundColor: Colors.white,
-          backgroundColor: Color(0xFF7D79D0),
-        ),
-        onChanged: (c) {
-          setState(() {
-            _toDolist[index]["ok"] = c;
-            _saveData();
-          });
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on,
+                          size: 150.0, color: Colors.amber),
+                      buildTextField(
+                          "Reais", "R\$", realController, _realChanged),
+                      Divider(),
+                      buildTextField(
+                          "Dolares", "US\$", dolarController, _dolarChanged),
+                      Divider(),
+                      buildTextField(
+                          "Euros", "€", euroController, _euroChanged),
+                      Divider(),
+                      buildTextField(
+                          "Bitcoins", "₿", bitcoinController, _bitcoinChanged),
+                    ],
+                  ),
+                );
+              }
+          }
         },
       ),
-      onDismissed: (direction) {
-        setState(() {
-          _lastRemoved = Map.from(_toDolist[index]);
-          _lastRemovedPos = index;
-          _toDolist.removeAt(index);
-
-          _saveData();
-          final snack = SnackBar(
-            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
-            action: SnackBarAction(
-              label: "Desfazer",
-              onPressed: () {
-                setState(() {
-                  _toDolist.insert(_lastRemovedPos, _lastRemoved);
-                  _saveData();
-                });
-              },
-            ),
-            duration: Duration(seconds: 2),
-          );
-          ScaffoldMessenger.of(context).removeCurrentSnackBar();
-          ScaffoldMessenger.of(context).showSnackBar(snack);
-        });
-      },
     );
   }
+}
 
-  Future<File> _getFile() async {
-    final directory = await getApplicationDocumentsDirectory();
-    return File("${directory.path}/data.json");
-  }
-
-  Future<File> _saveData() async {
-    String data = json.encode(_toDolist);
-    final file = await _getFile();
-    return file.writeAsString(data);
-  }
-
-  Future<String> _readData() async {
-    try {
-      final file = await _getFile();
-      return file.readAsString();
-    } catch (e) {
-      return null;
-    }
-  }
+Widget buildTextField(
+    String label, String prefix, TextEditingController c, Function f) {
+  return TextField(
+    controller: c,
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.amber),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+    ),
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    onChanged: f,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+  );
 }
